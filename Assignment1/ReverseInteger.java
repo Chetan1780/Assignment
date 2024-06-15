8643432888d960008c6b2afc80cd610cfd817004
Chetan1780 <122739438+Chetan1780@users.noreply.github.com>
2024-06-15 23:35:39 +0530

    Update ReverseInteger.java

--- Assignment1/ReverseInteger.java
+++ Assignment1/ReverseInteger.java
@@ -1,1 +1,19 @@
-
+class Solution {
+    public int reverse(int x) {
+        long number = 0;
+        boolean negative = false;
+        if(x<0){
+            x*=-1;
+            negative = true;
+        }
+        while(x>0){
+            int temp = x%10;
+            number = number*10 + (long)temp;
+            x/=10;
+        }
+        if(negative) number*=-1;
+        if(number>2147483647 || number<-2147483648)
+            return 0;
+        else return (int)number;
+    }
+}
